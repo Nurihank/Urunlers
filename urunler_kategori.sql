89e186526fa02efca989837f48203479991bfccd
Nurihank <kavalcinurihan@gmail.com>
2024-06-25 16:37:58 +0300

    sql

--- urunler_kategori.sql
+++ urunler_kategori.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: urunler
 -- ------------------------------------------------------
--- Server version	8.0.34
+-- Server version	8.0.37
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,30 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `urunler`
+-- Table structure for table `kategori`
 --
 
-DROP TABLE IF EXISTS `urunler`;
+DROP TABLE IF EXISTS `kategori`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `urunler` (
-  `idUrunler` int NOT NULL AUTO_INCREMENT,
-  `urunAdi` varchar(45) DEFAULT NULL,
-  `urunAciklamasi` varchar(45) DEFAULT NULL,
-  `urunlerResmi` varchar(255) DEFAULT NULL,
-  `urunlerUzunAciklama` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`idUrunler`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `kategori` (
+  `idkategori` int NOT NULL AUTO_INCREMENT,
+  `kategoriAdi` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idkategori`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `urunler`
+-- Dumping data for table `kategori`
 --
 
-LOCK TABLES `urunler` WRITE;
-/*!40000 ALTER TABLE `urunler` DISABLE KEYS */;
-INSERT INTO `urunler` VALUES (4,'Çok Fonksiyonlu Çaki','Çok Fonksiyonlu Çakı',NULL,NULL),(5,'İsviçre Çakisi','İsviçre Çakısı',NULL,NULL),(6,'Renkli Çaki','Renkli Çakı',NULL,NULL),(7,'Tam Otomatik Çadir','Tam Otomatik Çadır',NULL,NULL),(20,'Kelebek','Sallamali Bicak','../assets/kelebek.jpg','mesele çok uzun');
-/*!40000 ALTER TABLE `urunler` ENABLE KEYS */;
+LOCK TABLES `kategori` WRITE;
+/*!40000 ALTER TABLE `kategori` DISABLE KEYS */;
+INSERT INTO `kategori` VALUES (1,'Çadır'),(2,'Sandalye'),(3,'Tulum'),(5,'Çanta'),(6,'Sapka');
+/*!40000 ALTER TABLE `kategori` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-13 22:32:08
+-- Dump completed on 2024-06-25 16:27:34
